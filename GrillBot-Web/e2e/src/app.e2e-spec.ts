81bd3533bb545fb20b6af74b6f905df731727d90
Michal Halabica <m.halabica@gmail.com>
2019-08-18 09:13:04 +0200

    Removed old files. Repair naming convention

--- GrillBot-Web/e2e/src/app.e2e-spec.ts
+++ GrillBot-Web/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to GrilBotWeb!');
+    expect(page.getTitleText()).toEqual('Welcome to GrillBotWeb!');
   });
 
   afterEach(async () => {
