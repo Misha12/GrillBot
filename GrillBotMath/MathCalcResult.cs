3d55fd37108c1434b4735dbbc596a04336671e5f
Michal Halabica <m.halabica@gmail.com>
2019-08-26 20:52:11 +0200

    Rewrite math module to separate process.

---
+++ GrillBotMath/MathCalcResult.cs
@@ -0,0 +1,10 @@
+﻿namespace GrillBotMath
+{
+    public class MathCalcResult
+    {
+        public bool IsValid { get; set; }
+        public string ErrorMessage { get; set; }
+        public double Result { get; set; }
+        public double ComputingTime { get; set; }
+    }
+}
--- Grillbot/Services/MathCalculator.cs
+++ Grillbot/Services/MathCalculator.cs
@@ -1,9 +1,11 @@
 ﻿using Discord.WebSocket;
 using Grillbot.Models;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using org.mariuszgromada.math.mxparser;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,87 +22,37 @@ namespace Grillbot.Services
 
         public MathCalcResult Solve(string input, SocketUserMessage message)
         {
-            var expressionFields = input.Split(';').Select(o => o.Trim());
-            var arguments = ParseArguments(expressionFields);
-            var expressionData = expressionFields.FirstOrDefault(o => !IsVariableDeclaration(o));
-
-            if (string.IsNullOrEmpty(expressionData))
+            if (string.IsNullOrEmpty(input))
                 return new MathCalcResult(message?.Author.Mention, "Nelze spočítat prázdný požadavek.");
 
-            if (expressionData.Contains("nan", StringComparison.InvariantCultureIgnoreCase))
+            if (input.Contains("nan", StringComparison.InvariantCultureIgnoreCase))
                 return new MathCalcResult(message?.Author.Mention, "Toho bys asi chtěl moc.");
 
-            var expression = new Expression(expressionData, arguments.ToArray());
-            var errorMessages = ValidateAndGetErrors(expression);
-
-            if (errorMessages.Count > 0)
-                return new MathCalcResult(message.Author.Mention, string.Join(Environment.NewLine, errorMessages));
-
-            var calcTask = Task.Run(() =>
-            {
-                var result = expression.calculate();
-                var time = expression.getComputingTime();
-                return new Tuple<double, double>(result, time);
-            });
+            var appPath = Config["MethodsConfig:Math:ProcessPath"];
             var calcTime = Convert.ToInt32(Config["MethodsConfig:Math:ComputingTime"]);
 
-            if (!calcTask.Wait(calcTime))
-                return new MathCalcResult(message.Author.Mention, "Vypršel mi časový limit na výpočet příkladu.");
-
-            var calcTaskResult = calcTask.Result;
-            return new MathCalcResult(message.Author.Mention, calcTaskResult.Item1, calcTaskResult.Item2);
-        }
-
-        public List<string> ValidateAndGetErrors(Expression expression)
-        {
-            var errorMessages = new List<string>();
-
-            if (!expression.checkLexSyntax() || !expression.checkSyntax())
+            using (var process = new Process())
             {
-                if (!CheckMissingParameters(expression, out string errorMessage))
-                    errorMessages.Add(errorMessage);
-                else if (!CheckMissingFunctions(expression, out errorMessage))
-                    errorMessages.Add(errorMessage);
-                else
-                    errorMessages.Add("Syntax error");
+                process.StartInfo.FileName = "dotnet";
+                process.StartInfo.Arguments = $"{appPath} \"{input}\"";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                process.Start();
+                
+                if(!process.WaitForExit(calcTime))
+                {
+                    process.Kill();
+                    return new MathCalcResult(message.Author.Mention, "Vypršel mi časový limit na výpočet příkladu.");
+                }
+
+                var output = process.StandardOutput.ReadToEnd();
+                var data = JsonConvert.DeserializeObject<MathCalcResult>(output);
+
+                data.Mention = message.Author.Mention;
+                return data;
             }
-
-            return errorMessages;
-        }
-
-        private bool MissingDataCheck(Expression expression, Func<Expression, string[]> func, string errorMessageTemplate, out string errorMessage)
-        {
-            var missing = func(expression);
-
-            if(missing.Length > 0)
-            {
-                errorMessage = string.Format(errorMessageTemplate, string.Join(", ", missing));
-                return false;
-            }
-
-            errorMessage = null;
-            return true;
-        }
-
-        private bool CheckMissingParameters(Expression expression, out string errorMessage)
-        {
-            return MissingDataCheck(expression, e => e.getMissingUserDefinedArguments(), "Chybí mi parametr: {0}", out errorMessage);
-        }
-
-        private bool CheckMissingFunctions(Expression expression, out string errorMessage)
-        {
-            return MissingDataCheck(expression, o => o.getMissingUserDefinedFunctions(), "Chybí mi funkce: {0}", out errorMessage);
-        }
-
-        private bool IsVariableDeclaration(string field) => field.Trim().StartsWith("var", StringComparison.InvariantCultureIgnoreCase);
-
-        private List<Argument> ParseArguments(IEnumerable<string> expressionFields)
-        {
-            return expressionFields
-                .Where(IsVariableDeclaration)
-                .Select(o => o.Substring(4).Split('='))
-                .Select(o => new Argument(o[0].Trim(), Convert.ToDouble(o[1].Trim())))
-                .ToList();
         }
     }
 }
