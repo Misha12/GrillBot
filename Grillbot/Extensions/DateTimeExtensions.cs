2c75cb15a1ddf6f42445efe20e61c5c0ad2919ec
Michal Halabica <m.halabica@gmail.com>
2019-09-03 03:56:53 +0200

    Unique datetime formatting.

---
+++ Grillbot/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Grillbot.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToLocaleDatetime(this DateTime dateTime) => dateTime.ToString("dd. MM. yyyy HH:mm:ss");
+    }
+}
