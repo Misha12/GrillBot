e6617c3c278e3ae710a3668fb7c45e44915cc81f
Michal Halabica <m.halabica@gmail.com>
2019-09-18 17:15:44 +0200

    Generate config if appsettings.json not exists.

--- Grillbot/Program.cs
+++ Grillbot/Program.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using Grillbot.Services.Config.Models;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace Grillbot
 {
@@ -8,11 +12,57 @@ namespace Grillbot
     {
         public static void Main(string[] args)
         {
+            if (!ExistsConfig()) GenerateConfig();
+
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<AppStartup>()
                 .ConfigureLogging(o => o.SetMinimumLevel(LogLevel.Warning))
                 .Build()
                 .Run();
+        }
+
+        public const string AppSettingsFilename = "appsettings.json";
+
+        private static bool ExistsConfig() => File.Exists(AppSettingsFilename);
+
+        private static void GenerateConfig()
+        {
+            Console.WriteLine("appsettings.json file not found.");
+
+            var discordToken = GetValueFromInput("Insert token of discord development server: ");
+            var databaseConnectionString = GetValueFromInput("Insert database connection string: ");
+            var mathModulePath = GetValueFromInput("Insert GrillBotMath.dll path, optional: ", true);
+
+            var defaultConfig = Configuration.GenerateDefault(discordToken, databaseConnectionString, mathModulePath);
+
+            var jsonSettings = new JsonSerializerSettings()
+            {
+                DefaultValueHandling = DefaultValueHandling.Include,
+                NullValueHandling = NullValueHandling.Include,
+                Formatting = Formatting.Indented
+            };
+
+            var json = JsonConvert.SerializeObject(defaultConfig, jsonSettings);
+            Console.WriteLine("Default config generated. Starting.");
+
+            File.WriteAllText(AppSettingsFilename, json);
+        }
+
+        private static string GetValueFromInput(string message, bool optional = false)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                var result = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(result) && !optional)
+                {
+                    Console.WriteLine("Invalid input, try again.");
+                    continue;
+                }
+
+                return result;
+            }
         }
     }
 
--- Grillbot/Services/Config/Models/Configuration.cs
+++ Grillbot/Services/Config/Models/Configuration.cs
@@ -27,5 +27,64 @@ namespace Grillbot.Services.Config.Models
         public bool IsUserBotAdmin(ulong id) => Administrators.Any(o => o == id.ToString());
 
         public string GetValue(string route) => this.GetPropertyValue(route)?.ToString();
+
+        public static Configuration GenerateDefault(string token, string connectionString, string mathDllPath)
+        {
+            return new Configuration()
+            {
+                Administrators = new List<string>(),
+                AllowedHosts = "*",
+                CommandPrefix = "!",
+                Database = connectionString,
+                Discord = new DiscordConfig()
+                {
+                    Activity = "Help is !grillhelp",
+                    LoggerRoomID = "",
+                    Token = token,
+                    UserJoinedMessage = "Vítej"
+                },
+                EmoteChain_CheckLastCount = 5,
+                Log = new BotLogConfig() { LogRoomID = "" },
+                MethodsConfig = new MethodsConfig()
+                {
+                    AutoReply = new AutoReplyConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    Channelboard = new ChannelboardConfig()
+                    {
+                        Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } },
+                        WebTokenValidMinutes = 60,
+                        WebUrl = "http://localhost:4200/channelboard?token={0}"
+                    },
+                    EmoteManager = new EmoteManagerConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    Greeting = new GreetingConfig()
+                    {
+                        Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } },
+                        MessageTemplate = "Ahoj, {person}",
+                        OutputMode = GreetingOutputModes.Text
+                    },
+                    GrillStatus = new GrillStatusConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    Help = new HelpConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    Math = new MathConfig()
+                    {
+                        Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } },
+                        ComputingTime = 100000,
+                        ProcessPath = mathDllPath
+                    },
+                    MemeImages = new MemeImagesConfig()
+                    {
+                        Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } },
+                        AllowedImageTypes = new List<string>() { ".jpg", ".png", ".gif" },
+                        NotNudesDataPath = "",
+                        NudesDataPath = ""
+                    },
+                    ModifyConfig = new ModifyConfigConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    RoleManager = new RoleManagerConfig() { Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } } },
+                    TeamSearch = new TeamSearchConfig()
+                    {
+                        GeneralCategoryID = 0,
+                        Permissions = new PermissionsConfig() { RequiredRoles = new List<string>() { "dev" } }
+                    }
+                }
+            };
+        }
     }
 }
