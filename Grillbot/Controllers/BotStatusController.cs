d3d7d041f6ce79c8418f63ef39c347b46929fd72
Michal Halabica <m.halabica@gmail.com>
2019-10-12 22:24:08 +0200

    Created interface for REST API

---
+++ Grillbot/Controllers/BotStatusController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grillbot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotStatusController : ControllerBase
+    {
+        private void CheckAuth()
+        {
+            //TODO
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetSimpleStatus()
+        {
+            CheckAuth();
+
+            //TODO
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetCallStats()
+        {
+            CheckAuth();
+
+            // TODO
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetLoggerStats()
+        {
+            CheckAuth();
+
+            // TODO
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetTempUnverifyLog()
+        {
+            CheckAuth();
+
+            //TODO
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetSimpleAutoReplyStats()
+        {
+            CheckAuth();
+
+            // TODO
+            return Ok();
+        }
+    }
+}
