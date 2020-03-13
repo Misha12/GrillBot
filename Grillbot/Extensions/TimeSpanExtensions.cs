586bc4c4d73742a4f926d21ba22c55362be9953e
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2020-03-14 00:57:10 +0100

    Add module for fetching Kachna status from IsKachnaOpen

---
+++ Grillbot/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Grillbot.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static string ToCzechLongTimeString(this TimeSpan ts)
+        {
+            var hoursCzMod = ts.Hours == 1 ? "a" : (ts.Hours > 1 && ts.Hours < 5) ? "y" : "";
+            var minCzMod = ts.Minutes == 1 ? "a" : (ts.Minutes > 1 && ts.Minutes < 5) ? "y" : "";
+            return $"{ts.Hours} hodin{hoursCzMod} a {ts.Minutes} minut{minCzMod}";
+        }
+    }
+}
---
+++ Grillbot/Models/Duck/CurrentState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Grillbot.Models.Duck
+{
+    public class CurrentState
+    {
+        [JsonProperty(Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public DuckState State { get; set; }
+
+        public DateTime LastChange { get; set; }
+        public DateTime? ExpectedEnd { get; set; }
+        public string[] BeersOnTap { get; set; }
+        public string OpenedByName { get; set; }
+        public string OpenedByDiscordGlobalNick { get; set; }
+        public string EventName { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public DuckState NextPlannedState { get; set; }
+
+        public DateTime? NextStateDateTime { get; set; }
+        public DateTime? NextOpeningDateTime { get; set; }
+        
+        public string Note { get; set; }
+    }
+}
