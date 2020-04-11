c15dac5de608e0b50394a6209a6d686aa1730a99
Michal Halabica <m.halabica@gmail.com>
2020-04-11 23:52:12 +0200

    Issue #112: Multiple same events at same time.

--- Grillbot/Services/Logger/Logger.cs
+++ Grillbot/Services/Logger/Logger.cs
@@ -22,6 +22,10 @@ namespace Grillbot.Services.Logger
 
         public Dictionary<string, uint> Counters { get; }
 
+        private DateTime LastEventAt { get; set; }
+        private string LastEvent { get; set; }
+        private readonly object LastEventLock = new object();
+
         public Logger(DiscordSocketClient client, IOptions<Configuration> config, IMessageCache messageCache, ILogger<Logger> logger)
         {
             Client = client;
@@ -36,47 +40,57 @@ namespace Grillbot.Services.Logger
 
         public async Task OnGuildMemberUpdatedAsync(SocketGuildUser guildUserBefore, SocketGuildUser guildUserAfter)
         {
+            if (!CanProcessEvent("GuildMemberUpdated")) return;
+
             var method = new GuildMemberUpdated(Client, Config);
             var result = await method.ProcessAsync(guildUserBefore, guildUserAfter).ConfigureAwait(false);
 
             if (result)
-                IncrementEventHandle("GuildMemberUpdated");
+                EventPostProcess("GuildMemberUpdated");
         }
 
         public async Task OnMessageDelete(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel)
         {
+            if (!CanProcessEvent("MessageDeleted")) return;
+
             var method = new MessageDeleted(Client, Config, MessageCache, HttpClient, AppLogger);
             await method.ProcessAsync(message, channel).ConfigureAwait(false);
 
-            IncrementEventHandle("MessageDeleted");
+            EventPostProcess("MessageDeleted");
         }
 
         public async Task OnMessageEdited(Cacheable<IMessage, ulong> messageBefore, SocketMessage messageAfter, ISocketMessageChannel channel)
         {
+            if (!CanProcessEvent("MessageEdited")) return;
+
             var method = new MessageEdited(Client, Config, MessageCache);
             var result = await method.ProcessAsync(messageBefore, messageAfter, channel).ConfigureAwait(false);
 
             if (result)
-                IncrementEventHandle("MessageEdited");
+                EventPostProcess("MessageEdited");
         }
 
         public async Task OnUserJoined(SocketGuildUser user)
         {
+            if (!CanProcessEvent("UserJoined")) return;
+
             var method = new UserJoined(Client, Config);
             await method.ProcessAsync(user).ConfigureAwait(false);
 
-            IncrementEventHandle("UserJoined");
+            EventPostProcess("UserJoined");
         }
 
         public async Task OnUserLeft(SocketGuildUser user)
         {
+            if (!CanProcessEvent("UserLeft")) return;
+
             var method = new UserLeft(Client, Config);
             await method.ProcessAsync(user).ConfigureAwait(false);
 
-            IncrementEventHandle("UserLeft");
+            EventPostProcess("UserLeft");
         }
 
-        private void IncrementEventHandle(string name)
+        private void EventPostProcess(string name)
         {
             AppLogger.LogInformation($"Logger event {name} triggered.");
 
@@ -84,12 +98,26 @@ namespace Grillbot.Services.Logger
                 Counters.Add(name, 1);
             else
                 Counters[name]++;
+
+            lock (LastEventLock)
+            {
+                LastEvent = name;
+                LastEventAt = DateTime.UtcNow;
+            }
         }
 
         public void Dispose()
         {
             HttpClient.Dispose();
             Counters.Clear();
+        }
+
+        private bool CanProcessEvent(string @event)
+        {
+            lock (LastEventLock)
+            {
+                return @event != LastEvent || (DateTime.UtcNow - LastEventAt).TotalSeconds > 1;
+            }
         }
     }
 }
