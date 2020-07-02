57644dcb0794233a9fff03246f997fcb91c3f60d
MichalHalabica <m.halabica@gmail.com>
2020-07-02 22:50:45 +0200

    FormatHelper as extension method.

--- Grillbot/Extensions/NumberExtensions.cs
+++ Grillbot/Extensions/NumberExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Grillbot.Extensions
@@ -25,6 +26,21 @@ namespace Grillbot.Extensions
             nfi.NumberGroupSeparator = " ";
 
             return nfi;
+        }
+
+        public static string FormatAsSize(this long bytes)
+        {
+            if (bytes < 0)
+                throw new ArgumentException("Size must be positive");
+
+            if (bytes == 0)
+                return "0 B";
+
+            string[] sizes = new[] { "B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            var factor = (int)Math.Floor((bytes.ToString().Length - 1) / 3.0D);
+            var finalSize = bytes / Math.Pow(1024, factor);
+
+            return $"{Math.Round(finalSize, 2)} {sizes[factor]}";
         }
     }
 }
