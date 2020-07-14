1b8a8b3bdbc01bc936955b3918411333239a3577
MichalHalabica <m.halabica@gmail.com>
2020-07-14 20:09:21 +0200

    Removed Math from webadmin

--- Grillbot/Database/Repository/MathRepository.cs
+++ Grillbot/Database/Repository/MathRepository.cs
@@ -14,41 +14,6 @@ namespace Grillbot.Database.Repository
         {
         }
 
-        public IQueryable<MathAuditLogItem> GetAuditLog(MathAuditLogFilter filter)
-        {
-            var query = Context.MathAuditLogs
-                .Include(o => o.User)
-                .OrderByDescending(o => o.ID)
-                .AsQueryable();
-
-            if (filter.Channel != null)
-            {
-                var channel = filter.Channel.Value.ToString();
-                query = query.Where(o => o.ChannelID == channel);
-            }
-
-            if (filter.DateTimeFrom != null)
-                query = query.Where(o => o.DateTime >= filter.DateTimeFrom.Value);
-
-            if (filter.DateTimeTo != null)
-                query = query.Where(o => o.DateTime < filter.DateTimeTo.Value);
-
-            if (filter.GuildID != null)
-            {
-                var guild = filter.GuildID.Value.ToString();
-                query = query.Where(o => o.User.GuildID == guild);
-            }
-
-            if (filter.UserID != null)
-            {
-                var user = filter.UserID.Value.ToString();
-                query = query.Where(o => o.User.UserID == user);
-            }
-
-            var skip = (filter.Page == 0 ? 0 : filter.Page - 1) * PageSize;
-            return query.Skip(skip).Take(PageSize);
-        }
-
         public IQueryable<MathAuditLogItem> GetLogData(MathHistoryFilter filter, SocketGuild guild)
         {
             var query = Context.MathAuditLogs
