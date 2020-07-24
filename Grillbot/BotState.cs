b9a9a9d984ebc20a8a96041f2088c0bc0ebb2346
MichalHalabica <m.halabica@gmail.com>
2020-07-24 21:31:35 +0200

    Bot state service

---
+++ Grillbot/BotState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grillbot
+{
+    public class BotState
+    {
+        public Dictionary<string, DateTime> LastPointsCalculation { get; set; }
+
+        public BotState()
+        {
+            LastPointsCalculation = new Dictionary<string, DateTime>();
+        }
+    }
+}
