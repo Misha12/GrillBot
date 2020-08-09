830b8cba99af9e71a0adb2db939cd4c4a572c606
MichalHalabica <m.halabica@gmail.com>
2020-08-09 16:58:49 +0200

    Unverify: Evaluate the roles and channels to be removed.

--- Grillbot/Services/Unverify/Models/ChannelOverwrite.cs
+++ Grillbot/Services/Unverify/Models/ChannelOverwrite.cs
@@ -1,10 +1,14 @@
 using Discord;
+using Newtonsoft.Json;
 
 namespace Grillbot.Services.Unverify.Models
 {
     public class ChannelOverwrite
     {
         public IChannel Channel { get; set; }
-        public OverwritePermissions Permissions { get; set; }
+        public OverwritePermissions? Permissions { get; set; }
+
+        [JsonIgnore]
+        public OverwritePermissions Perms => Permissions.Value;
     }
 }
---
+++ Grillbot/Services/Unverify/UnverifyService.cs
@@ -0,0 +1,18 @@
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grillbot.Services.Unverify
+{
+    public class UnverifyService
+    {
+        
+
+        public async Task SetUnverify(List<SocketGuildUser> users, SocketGuild guild, string time, string data, bool isSelfUnverify)
+        {
+
+        }
+    }
+}
