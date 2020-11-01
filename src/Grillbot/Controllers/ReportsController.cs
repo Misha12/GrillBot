9603659aa54eb6aaea37f433d0b3d7a2cbf8161c
Michal Halabica <m.halabica@gmail.com>
2020-11-01 17:02:08 +0100

    Moved report module to web admin. Resolved #194

---
+++ src/Grillbot/Controllers/ReportsController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Grillbot.Models.BotStatus;
+using Grillbot.Services;
+using Grillbot.Services.Statistics;
+using Grillbot.Services.Statistics.ApiStats;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grillbot.Controllers
+{
+    [Authorize]
+    [Route("Admin/Reports")]
+    public class ReportsController : Controller
+    {
+        private BotStatusService StatusService { get; }
+        private InternalStatistics InternalStatistics { get; }
+        private ApiStatistics ApiStatistics { get; }
+        private DiscordSocketClient DiscordClient { get; }
+
+        public ReportsController(BotStatusService statusService, InternalStatistics internalStatistics, ApiStatistics apiStatistics,
+            DiscordSocketClient discordClient)
+        {
+            StatusService = statusService;
+            InternalStatistics = internalStatistics;
+            ApiStatistics = apiStatistics;
+            DiscordClient = discordClient;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var getDbStatusTask = StatusService.GetDbReport();
+
+            var result = new ReportsViewModel()
+            {
+                BotStatus = StatusService.GetSimpleStatus(),
+                Commands = InternalStatistics.GetCommands(),
+                Events = InternalStatistics.GetEvents(),
+                Logger = StatusService.GetLoggerStats(),
+                GCMemoryInfo = GC.GetGCMemoryInfo(),
+                Api = ApiStatistics.Data.FindAll(o => o.Count > 0),
+                LoginState = DiscordClient.LoginState,
+                ConnectionState = DiscordClient.ConnectionState,
+                Latency = DiscordClient.Latency
+            };
+
+            result.Database = await getDbStatusTask;
+            return View(result);
+        }
+    }
+}
---
+++ src/Grillbot/Models/BotStatus/ReportsViewModel.cs
@@ -0,0 +1,21 @@
+using Discord;
+using Grillbot.Services.Statistics.ApiStats;
+using System;
+using System.Collections.Generic;
+
+namespace Grillbot.Models.BotStatus
+{
+    public class ReportsViewModel
+    {
+        public Dictionary<string, ulong> Commands { get; set; }
+        public Dictionary<string, uint> Logger { get; set; }
+        public Dictionary<string, ulong> Events { get; set; }
+        public Dictionary<string, Tuple<int, long>> Database { get; set; }
+        public GCMemoryInfo GCMemoryInfo { get; set; }
+        public SimpleBotStatus BotStatus { get; set; }
+        public List<ApiStatsData> Api { get; set; }
+        public ConnectionState ConnectionState { get; set; }
+        public int Latency { get; set; }
+        public LoginState LoginState { get; set; }
+    }
+}
