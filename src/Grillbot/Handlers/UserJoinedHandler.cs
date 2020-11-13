e72de4e5596d4e9dc75197e15bded77e46e4486a
Michal Halabica <m.halabica@gmail.com>
2020-11-14 00:53:27 +0100

    End of support of UserJoinedMessage.

--- src/Grillbot/Handlers/UserJoinedHandler.cs
+++ src/Grillbot/Handlers/UserJoinedHandler.cs
@@ -1,42 +1,33 @@
 using System;
 using System.Threading.Tasks;
 using Discord.WebSocket;
-using Grillbot.Extensions.Discord;
-using Grillbot.Models.Config.AppSettings;
 using Grillbot.Services.Initiable;
 using Grillbot.Services.InviteTracker;
 using Grillbot.Services.Logger;
 using Grillbot.Services.Statistics;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Grillbot.Handlers
 {
     public class UserJoinedHandler : IDisposable, IInitiable
     {
         private DiscordSocketClient Client { get; }
-        private Configuration Config { get; }
         private Logger Logger { get; }
         private InternalStatistics InternalStatistics { get; }
         private IServiceProvider Services { get; }
 
-        public UserJoinedHandler(DiscordSocketClient client, IOptions<Configuration> config, Logger logger, InternalStatistics internalStatistics,
+        public UserJoinedHandler(DiscordSocketClient client, Logger logger, InternalStatistics internalStatistics,
             IServiceProvider services)
         {
             Client = client;
             Logger = logger;
             InternalStatistics = internalStatistics;
-            Config = config.Value;
             Services = services;
         }
 
         private async Task OnUserJoinedOnServerAsync(SocketGuildUser user)
         {
             InternalStatistics.IncrementEvent("UserJoined");
-            var message = Config.Discord.UserJoinedMessage;
-
-            if (!string.IsNullOrEmpty(message))
-                await user.SendPrivateMessageAsync(message).ConfigureAwait(false);
 
             await Logger.OnUserJoined(user).ConfigureAwait(false);
             await ProcessInviteTrackerAsync(user);
