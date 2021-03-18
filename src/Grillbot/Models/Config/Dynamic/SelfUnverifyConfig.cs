9ec3a3b3b233d1e65d75ab4eadc716428be33db9
Jindřich Dítě <adrijaned@gmail.com>
2021-03-18 23:58:59 +0100

    Introduced a concept of "discouraged" selfunverifyers, who have their minimum selfunverify time silently increased to 6h.
    Kuzník will be Kuzník.

--- src/Grillbot/Models/Config/Dynamic/SelfUnverifyConfig.cs
+++ src/Grillbot/Models/Config/Dynamic/SelfUnverifyConfig.cs
@@ -6,10 +6,12 @@ namespace Grillbot.Models.Config.Dynamic
     {
         public int MaxRolesToKeep { get; set; }
         public Dictionary<string, List<string>> RolesToKeep { get; set; }
+        public List<ulong> DiscouragedUsers { get; set; }
 
         public SelfUnverifyConfig()
         {
             RolesToKeep = new Dictionary<string, List<string>>();
+            DiscouragedUsers = new List<ulong>();
         }
     }
 }
