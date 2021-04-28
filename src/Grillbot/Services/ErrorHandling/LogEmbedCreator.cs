800d411b62f337463705473faa3a3ace140d6a21
Michal Halabica <m.halabica@gmail.com>
2021-04-28 22:29:35 +0200

    Removed error logging to database.

--- src/Grillbot/Services/ErrorHandling/LogEmbedCreator.cs
+++ src/Grillbot/Services/ErrorHandling/LogEmbedCreator.cs
@@ -1,9 +1,7 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Grillbot.Database.Entity;
 using Grillbot.Models.Embed;
-using System;
 
 namespace Grillbot.Services.ErrorHandling
 {
@@ -16,30 +14,28 @@ namespace Grillbot.Services.ErrorHandling
             DiscordClient = discordClient;
         }
 
-        public BotEmbed CreateErrorEmbed(LogMessage message, ErrorLogItem logItem)
+        public BotEmbed CreateErrorEmbed(LogMessage message)
         {
             if (message.Exception is CommandException commandException)
             {
-                return CreateCommandErrorEmbed(commandException, logItem);
+                return CreateCommandErrorEmbed(commandException);
             }
 
-            return CreateGenericErrorEmbed(logItem, message.Source);
+            return CreateGenericErrorEmbed(message.Source);
         }
 
-        private BotEmbed CreateCommandErrorEmbed(CommandException exception, ErrorLogItem logItem)
+        private BotEmbed CreateCommandErrorEmbed(CommandException exception)
         {
             return new BotEmbed(DiscordClient.CurrentUser, Color.Red, "Při provádění příkazu došlo k chybě")
-                .AddField("ID záznamu", logItem.ID.ToString(), true)
                 .AddField("Kanál", $"<#{exception.Context.Channel.Id}>", true)
                 .AddField("Uživatel", exception.Context.User.Mention, true)
                 .AddField("Zpráva", $"```{exception.Context.Message.Content}```", false)
                 .AddField("Skok na zprávu", exception.Context.Message.GetJumpUrl(), false);
         }
 
-        private BotEmbed CreateGenericErrorEmbed(ErrorLogItem logItem, string source)
+        private BotEmbed CreateGenericErrorEmbed(string source)
         {
             return new BotEmbed(DiscordClient.CurrentUser, Color.Red, "Došlo k neočekávané chybě.")
-                .AddField("ID záznamu", logItem.ID.ToString(), true)
                 .AddField("Zdroj", source, true);
         }
     }
