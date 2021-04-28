cc1651333fde9ceee7af3f67bf418399ab2fe3c0
Michal Halabica <m.halabica@gmail.com>
2021-04-28 23:18:11 +0200

    Added migrations

---
+++ src/Grillbot/Migrations/20210428211718_Optimalizations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Grillbot.Migrations
+{
+    public partial class Optimalizations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Errors");
+
+            migrationBuilder.DropTable(
+                name: "GlobalConfig");
+
+            migrationBuilder.DropColumn(
+                name: "UsedCount",
+                table: "MethodsConfig");
+
+            migrationBuilder.DropColumn(
+                name: "ApiAccessCount",
+                table: "DiscordUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UsedCount",
+                table: "MethodsConfig",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApiAccessCount",
+                table: "DiscordUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Errors",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Errors", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GlobalConfig",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlobalConfig", x => x.Key);
+                });
+        }
+    }
+}
