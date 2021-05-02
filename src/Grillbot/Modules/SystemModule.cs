40380a4646f818d669c4725c1e27b39ee4361d04
Michal Halabica <m.halabica@gmail.com>
2021-05-02 22:40:10 +0200

    Added support of attachments for system send.

--- src/Grillbot/Modules/SystemModule.cs
+++ src/Grillbot/Modules/SystemModule.cs
@@ -1,36 +1,33 @@
 using Discord;
 using Discord.Commands;
 using Grillbot.Attributes;
-using Grillbot.Extensions.Discord;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Grillbot.Modules
 {
     [Group("system")]
     [Name("Interní správa bota")]
-    [ModuleID("SystemModule")]
+    [ModuleID(nameof(SystemModule))]
     public class SystemModule : BotModuleBase
     {
-        private ILogger<SystemModule> Logger { get; }
-        private IHostApplicationLifetime Lifetime { get; }
-        private BotState BotState { get; }
-
-        public SystemModule(ILogger<SystemModule> logger, IHostApplicationLifetime lifetime, BotState botState, IServiceProvider provider) : base(provider: provider)
-        {
-            Logger = logger;
-            Lifetime = lifetime;
-            BotState = botState;
-        }
-
         [Command("send")]
         [Summary("Odeslání zprávy do kanálu.")]
         public async Task SendAsync(ITextChannel textChannel, [Remainder] string message)
         {
-            Logger.LogInformation($"{Context.User.GetFullName()} send message to {textChannel.Name}. Content: {message}");
             await textChannel.SendMessageAsync(message);
+
+            using var httpClient = new HttpClient();
+            foreach (var attachment in Context.Message.Attachments)
+            {
+                using var response = await httpClient.GetAsync(attachment.Url);
+
+                if (!response.IsSuccessStatusCode)
+                    continue;
+
+                using var stream = await response.Content.ReadAsStreamAsync();
+                await textChannel.SendFileAsync(stream, attachment.Filename, isSpoiler: attachment.IsSpoiler());
+            }
         }
     }
 }
